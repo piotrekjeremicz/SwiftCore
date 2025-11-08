4f69bed8d93153259bf906338b5208a864a40b10
Piotr Jeremicz <piotrek@jeremicz.com>
2025-11-08 17:00:01 +0100

    Extend assembly with registrars

---
+++ Sources/Core/Dependency/Registrar.swift
@@ -0,0 +1,62 @@
+//
+//  Registrar.swift
+//  Core
+//
+//  Created by Piotrek Jeremicz on 28.10.2025.
+//
+
+public protocol Registrar {
+    var container: Container { get }
+    
+    func register<Service>(_ serviceType: Service.Type, factory: @Sendable @escaping (Resolver) -> Service)
+}
+
+
+public class StoreRegistrar: Registrar {
+    public let container: Container
+    
+    init(container: Container) {
+        self.container = container
+    }
+    
+    public func register<Service>(_ serviceType: Service.Type, factory: @escaping @Sendable (any Resolver) -> Service) {
+        container.register(
+            serviceType,
+            scope: .singleton,
+            factory: factory
+        )
+    }
+}
+
+
+public class RepositoryRegistrar: Registrar {
+    public let container: Container
+    
+    init(container: Container) {
+        self.container = container
+    }
+    
+    public func register<Service>(_ serviceType: Service.Type, factory: @escaping @Sendable (any Resolver) -> Service) {
+        container.register(
+            serviceType,
+            scope: .singleton,
+            factory: factory
+        )
+    }
+}
+
+public class UseCaseRegistrar: Registrar {
+    public let container: Container
+
+    init(container: Container) {
+        self.container = container
+    }
+    
+    public func register<Service>(_ serviceType: Service.Type, factory: @escaping @Sendable (any Resolver) -> Service) {
+        container.register(
+            serviceType,
+            scope: .graph,
+            factory: factory
+        )
+    }
+}
--- Sources/Core/Feature/Assembly/Assembly.swift
+++ Sources/Core/Feature/Assembly/Assembly.swift
@@ -7,8 +7,23 @@
 
 public protocol Assembly {
     func assemble(container: Container)
+    
+    func register(in container: Container)
+    func registerStores(in registrar: StoreRegistrar)
+    func registerRepositories(in registrar: RepositoryRegistrar)
+    func registerUseCases(in registrar: UseCaseRegistrar)
 }
 
 public extension Assembly {
-//    func load(resolver: String/*InjectResolver*/) {}
+    func assemble(container: Container) {
+        register(in: container)
+        registerStores(in: .init(container: container))
+        registerRepositories(in: .init(container: container))
+        registerUseCases(in: .init(container: container))
+    }
+    
+    func register(in container: Container) { }
+    func registerStores(in registrar: StoreRegistrar) { }
+    func registerRepositories(in registrar: RepositoryRegistrar) { }
+    func registerUseCases(in registrar: UseCaseRegistrar) { }
 }
