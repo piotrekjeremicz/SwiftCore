50c02ffcab72617c83eedb7a1441a8edbd3e2059
piotrekjeremicz <piotrek@jeremicz.com>
2025-12-24 13:55:25 +0100

    Publish CoreAssembly for DeveloperTools

--- Sources/Core/Assembly/CoreAssembly.swift
+++ Sources/Core/Assembly/CoreAssembly.swift
@@ -5,7 +5,9 @@
 //  Created by Piotrek Jeremicz on 20/12/2025.
 //
 
-final class CoreAssembly {
+public final class CoreAssembly {
+    public init() {}
+    
     public func register(in container: Container) {
         container.register(CoordinatorRegistrar.self, scope: .singleton) { _ in
             CoordinatorRegistrar()
