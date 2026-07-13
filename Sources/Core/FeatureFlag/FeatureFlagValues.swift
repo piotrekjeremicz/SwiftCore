ec76801f5791f6a9ea7e094c7c649f7b9f052879
Piotrek Jeremicz <piotrek@jeremicz.com>
2026-07-13 23:52:43 +0200

    Implemented `FeatureFlagValues` to represent the Flag based on the App Environment

---
+++ Sources/Core/FeatureFlag/FeatureFlagValues.swift
@@ -0,0 +1,30 @@
+//
+//  FeatureFlagValues.swift
+//  Core
+//
+//  Created by Piotrek Jeremicz on 13.07.2026.
+//
+
+public struct FeatureFlagValues: Sendable {
+    public let environment: AppEnvironment
+
+    public init(environment: AppEnvironment) {
+        self.environment = environment
+    }
+
+    public func flag<Flag: FeatureFlag>(
+        @FeatureFlagVariantBuilder<Flag> _ variants: () -> [FeatureFlagVariant<Flag>]
+    ) -> Flag {
+        let variants = variants()
+
+        if let match = variants.first(where: { $0.environment == environment }) {
+            return match.flag
+        }
+
+        if let fallback = variants.first(where: { $0.environment == nil }) {
+            return fallback.flag
+        }
+
+        fatalError("❌ No FeatureFlag variant of \(Flag.self) for environment '\(environment.rawValue)' and no .default provided")
+    }
+}
